c602bcfc25531765f0d1a285e5970cc2220b1473
Katie Schmitt <kschmit1@nd.edu>
2013-12-16 20:14:52 -0500

    Updated Java

--- javascript_for_final.js
+++ javascript_for_final.js
@@ -4,7 +4,25 @@
     $Santa.attr('alt', 'Santa in the day');
   });
   
-window.onload 
-console.log (window.innerWidth);
+ $(document).on('click','img', function() {
+  var $this = $(this);
+  console.log('alt: ', $(this).attr("alt"));
+	console.log('width:', $(this).width());
+	console.log('height:', $(this).height());
+	});
+
+ $(document).on('click', 'h1,h2,h3,h4', function() {
+  var $this = $(this);
+	console.log("Alt Text:", $(this).text());
+	console.log('color:', $(this).css('color'));
+	console.log('font-size:', $(this).css('color'));
+  });
+  
+ console.log($(window).width());
+
+ $(window).resize(function(){
+    $('width').text($(window).width());
+	});
+ 
 
   
