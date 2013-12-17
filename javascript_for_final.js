9da0b755e9708ade504b35056e676af4359e0844
Katie Schmitt <kschmit1@nd.edu>
2013-12-16 20:36:40 -0500

    Updated Java

--- javascript_for_final.js
+++ javascript_for_final.js
@@ -1,28 +1,28 @@
-  $('#Light_Him').on("click", function(){
-    var $Santa = $('#Santa_Unlit');
-    $Santa.attr('src', 'Santa_lit.jpeg');
-    $Santa.attr('alt', 'Santa in the day');
+  $("#Light_Him").on("click", function(){
+    var $Santa = $("#Santa_Unlit");
+    $Santa.attr("src", "Santa_lit.jpeg");
+    $Santa.attr("alt", "Santa in the day");
   });
   
- $(document).on('click','img', function() {
+ $(document).on("click","img", function() {
   var $this = $(this);
-  console.log('alt: ', $(this).attr("alt"));
-	console.log('width:', $(this).width());
-	console.log('height:', $(this).height());
+  console.log("Alt: ", $(this).attr("alt"));
+	console.log("width: ", $(this).width());
+	console.log("height: ", $(this).height());
 	});
 
- $(document).on('click', 'h1,h2', function() {
+ $(document).on("click", "h1,h2", function() {
   var $this = $(this);
-	console.log("Alt Text:", $(this).text());
-	console.log('color:', $(this).css('color'));
-	console.log('font-size:', $(this).css('color'));
+	console.log("alt text: ", $(this).text());
+	console.log("color: ", $(this).css('color'));
+	console.log("font-size: ", $(this).css('font-size'));
   });
   
- console.log($(window).width());
+ console.log("width: ", $(window).width());
 
  $(window).resize(function(){
-    $('width').text($(window).width());
-     console.log($(window).width());
+    $("width").text($(window).width());
+     console.log("Width: ", $(window).width());
 	});
 
 
